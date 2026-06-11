be7c2a674a2a840e43a40f5da3cdba70821065e9
sheikhsaqibfayaz11-stack <sheikhsaqib40@gmail.com>
2026-06-11 18:17:21 +0100

    Initial Commit

---
+++ oms-api/src/main/java/com/sheikh/oms_api/OmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sheikh.oms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //identifies as starting point of the application
+public class OmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsApiApplication.class, args);
+	}
+
+}
---
+++ oms-api/src/test/java/com/sheikh/oms_api/OmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheikh.oms_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
